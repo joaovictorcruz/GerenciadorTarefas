20445378889a47ec97a56ac73ad882e4a1f341a9
João Victor <elietejvc@hotmail.com>
2024-10-09 15:33:24 -0300

    Adicionando primeiros css tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import AddTask from "./components/AddTask";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text 3xl">Gerenciador de Tarefas</h1>
+    <div className="w-screen h-screen bg-gray-500 flex justify-center p-6">
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
       <Tasks/>
       <AddTask/>
     </div>
