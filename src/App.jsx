469f142282379cf3d96ebff6c49cb18f5fb8355d
João Victor <elietejvc@hotmail.com>
2024-10-09 18:15:25 -0300

    Estilizando tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Tasks from "./components/Task";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
-function App() {
+//color background-color gray-100 azul indigo-300 violet-300
+function App(){
+  const [tasks] = useState([
+  {
+    id: 1,
+    title: "Estudar programação",
+    description: "Estudar programção para se tornar um desenvolvedor Full Stack.",
+    isComplete: false,
+  },
+  {
+    id: 2,
+    title: "Estudar Matemática",
+    description: "Estudar equações e inequações",
+    isComplete: false,
+  },
+]);
   return (
-    <div className="w-screen h-screen bg-gray-500 flex justify-center p-6">
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <Tasks/>
+    <div className="w-screen h-screen bg-violet-300 flex justify-center p-6">
+      <div className="w-[500px]">
+      <h1 className="text-3xl text text-slate-100 font-bold text-center">Planejamento de Estudos</h1>
       <AddTask/>
+      <Tasks tasks={tasks}/>
+      </div>
     </div>
+
   );
 }
 
